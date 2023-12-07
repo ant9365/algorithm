5b11818c984f9fa04aaf4fef7b184816711766e0
donggyun_kim <donggyun_kim2@tmaxsoft.co.kr>
2023-12-07 06:05:45 +0000

    git add

---
+++ 11726/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n, i;
+    int d[1014];
+    scanf("%d",&n);
+    d[1] = 1;
+    d[2] = 2;
+    if(n >= 3) {
+        for(i = 3 ; i <= n ; i++) {
+            d[i] = (d[i-2] + d[i-1]) % 10007;
+        }
+    }
+    cout<<d[n]<<endl;
+}
+
---
+++ 14501/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int a[20], b[20];
+int result =0;
+void get(int now, int sum){
+
+	if(now == n + 1){
+		result = max(sum, result);
+		return;
+	}
+	get(now + 1, sum);
+	if(now + a[now] <= n + 1){
+		get(now + a[now],sum + b[now]);
+	}
+}
+int main(){
+	int n;
+	cin >> &n;
+	for(int i = 1; i <= n; i++){
+		cin >> &a[i] >> &b[i];
+	}
+	get(1, 0);
+	cout << result << endl;
+}
